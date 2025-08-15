864565b23df25eee9126d7b1dd03e47b804fd723
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-08-15 20:46:40 +0200

    fix(ci): fixed inexistant config file

--- src/test/test_panel.ts
+++ src/test/test_panel.ts
@@ -1,14 +1,13 @@
 import * as assert from "jsr:@std/assert";
 import "jsr:@std/dotenv/load";
 import { getNodes } from "../api/pterodactyl.ts";
-import config from "../../config.json" with { type: "json" };
 
 Deno.test({
   name: "request_panel",
   permissions: { env: true, net: true },
   fn: async () => {
-    const hostname = Deno.env.get("HOSTNAME") ?? config.hostname;
-    const apikey = Deno.env.get("APIKEY") ?? config.apikey;
+    const hostname = Deno.env.get("HOSTNAME");
+    const apikey = Deno.env.get("APIKEY");
 
     if (hostname == undefined || apikey == undefined) {
       console.error("Missing HOSTNAME or APIKEY in environment");
