ebb04bdf030f2dbf90bae6b320ddfd033a4c667e
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-08-15 20:44:21 +0200

    fix(ci): fixed test panel request (internal) & perm

--- src/test/test_panel.ts
+++ src/test/test_panel.ts
@@ -1,13 +1,14 @@
 import * as assert from "jsr:@std/assert";
 import "jsr:@std/dotenv/load";
 import { getNodes } from "../api/pterodactyl.ts";
+import config from "../../config.json" with { type: "json" };
 
 Deno.test({
-  name: "request",
-  permissions: { env: true, read: true, net: true },
+  name: "request_panel",
+  permissions: { env: true, net: true },
   fn: async () => {
-    const hostname = Deno.env.get("HOSTNAME");
-    const apikey = Deno.env.get("APIKEY");
+    const hostname = Deno.env.get("HOSTNAME") ?? config.hostname;
+    const apikey = Deno.env.get("APIKEY") ?? config.apikey;
 
     if (hostname == undefined || apikey == undefined) {
       console.error("Missing HOSTNAME or APIKEY in environment");
